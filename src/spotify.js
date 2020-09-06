7d32746bd76d7f4835bbb05de96663ea98b2ad27
Abey Thomas <abeytmas@gmail.com>
2020-09-07 00:47:54 +0530

    update redirect uri for spotify auth

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = 'e7f88dacdcb74b4899555e719111ac38';
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = window.location.origin + '/';
 const scopes = [
   'user-read-currently-playing',
   'user-read-recently-played',
@@ -10,7 +10,7 @@ const scopes = [
   'user-top-read',
   'user-modify-playback-state',
 ];
-
+console.log('window.location', window.location.origin)
 export const getTokenFromResponse = () => window.location.hash
   .substring(1)
   .split('&')
